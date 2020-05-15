80c5f9a9d598f8c6b8e040431da29f1e2df6a481
Anastasiya <amolchkova@mail.ru>
2020-05-15 14:09:47 +0700

    Добавьте файлы проекта.

---
+++ Deletion_of_other_signs.cpp
@@ -0,0 +1,28 @@
+﻿/* Напишите программу, которая удаляет из строки, введенной пользователем, все пробелы и знаки препинания.*/
+
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+inline bool sign_is_ok(const char sign)
+{
+    if ((sign == ' ') || (sign == '.') || (sign == ',') || (sign == '!') || (sign == '?') || (sign == ':') || (sign == ';') || (sign == '-') || (sign == '"'))
+        return false;
+    else return true;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Программа убирает знаки препинания и пробелы. Напишите что-нибудь:\n";
+    std::string text;
+    std::getline(std::cin, text);
+    std::cout << "Результат:\n";
+    for (const auto& sign : text)
+        if (sign_is_ok(sign))
+            std::cout << sign;
+    std::cout << std::endl;
+    return 0;
+}
